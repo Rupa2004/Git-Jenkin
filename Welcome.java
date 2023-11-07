467b511356913b04edd258982a10dd4e13457162
Rupa2004 <rupadandina@gmail.com>
2023-11-07 19:17:05 +0530

    This was commited by Rupa

--- Welcome.java
+++ Welcome.java
@@ -2,6 +2,6 @@ public class Welcome
 {
   public static void main(String args[])
   {
-     System.out.println("This program will run a code in Github on Jenkin Server")
+     System.out.println("This program will run a code in Github on Jenkin Server");
    }
 }
